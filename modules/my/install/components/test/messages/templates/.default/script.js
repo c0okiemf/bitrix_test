41f362f9ed7a9319c1d75c51be8eda3a789909b9
c0okiemf <48250391+c0okiemf@users.noreply.github.com>
2019-12-24 15:30:46 +0300

    Update script.js

--- modules/my/install/components/test/messages/templates/.default/script.js
+++ modules/my/install/components/test/messages/templates/.default/script.js
@@ -1,14 +1,15 @@
-$('#send-button').on('click', function () {
-    let message = $('#message-form').val();
-    if (message.length > 0) {
-        console.log(message);
-        $.ajax({
-            method: 'POST',
-            url: ajaxUrl,
-            data: {
-                'message': message
-            }
-        });
-        location.reload();
-    }
+$(document).ready(function() {
+    $('#send-button').on('click', function () {
+        let message = $('#message-form').val();
+        if (message.length > 0) {
+            $.ajax({
+                method: 'POST',
+                url: ajaxUrl,
+                data: {
+                    'message': message
+                }
+            });
+            location.reload();
+        }
+    });
 });
